0db225b02d6c0a70ec60073c92de135a3b8149ad
Raj Manvar <raj.manvar@students.iiit.ac.in>
2016-07-06 16:27:15 +0530

    debugging

--- annolet_main.js
+++ annolet_main.js
@@ -282,8 +282,8 @@ function rajfunc(event)
     var context = canvas.getContext('2d');
 //    var centerX = (event.clientX*1353)/1510;
     //    var centerY = (event.clientY*750)/671;
-    var centerX = canvas.width/2;
-    var centerY = canvas.height/2;
+    var centerX = canvas.width;
+    var centerY = canvas.height;
     var radius = 0.5;
 
 
