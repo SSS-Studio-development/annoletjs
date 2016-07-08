3d3397b4e76b0bfdac073b0c9d446b0799819512
Raj Manvar <raj.manvar@students.iiit.ac.in>
2016-07-08 15:05:18 +0530

    debugging

--- annolet_main.js
+++ annolet_main.js
@@ -210,7 +210,7 @@ function anno_rtag(xpath)
   {
       console.log("for adding jquery");
       script=document.createElement('script');
-      script.src='src="https://code.jquery.com/ui/1.9.1/jquery-ui.min.js"';
+      script.src='//code.jquery.com/ui/1.9.1/jquery-ui.min.js"';
       document.getElementsByTagName('head')[0].appendChild(script);
   }
 
