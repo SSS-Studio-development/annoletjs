362d97630a2576be0f50f6ee07949a085755fecb
Raj Manvar <raj.manvar@students.iiit.ac.in>
2016-07-11 11:45:18 +0530

    debugging

--- annolet_main.js
+++ annolet_main.js
@@ -226,7 +226,7 @@ function anno_rtag(xpath)
         console.log('highlighted');
         var popUpList = $j('<div><form><input type=\"radio\" name=\"r1\" value=\"A\" id=\"radio1\" checked=\"checked\">Date<br><label for="radio2">Currency</label> <input value=\"B\" type=\"radio\" name = \"r2\" id=\"radio2\"><br> <input type=\"radio\" name=\"r3\" id=\"radio3\" value=\"C\">Unit<br> <button id=\"submitunique\">submit</button></form></div>');
 //        popUpList.dialog();
-        document.getElementsByTagName('body')[0].appendChild(popUpList);
+        document.getElementsByTagName('body')[0].append(popUpList);
         $j("#submitunique").click(function(){
             console.log("submit clicked");
             alert($j("#radio:checked").val());});
